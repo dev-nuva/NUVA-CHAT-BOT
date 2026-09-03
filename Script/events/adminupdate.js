e2e6d9045fb4feebe9204aef4c959851b49d33fc
AJ Nuva <arifajahannuva1@gmail.com>
2026-09-03 23:02:15 +0600

    Update credits in adminUpdate.js

--- Script/events/adminupdate.js
+++ Script/events/adminupdate.js
@@ -2,7 +2,7 @@ module.exports.config = {
 name: "adminUpdate",
 eventType: ["log:thread-admins","log:thread-name", "log:user-nickname","log:thread-icon","log:thread-call","log:thread-color"],
 version: "1.0.0",
-credits: "SHAHADAT SAHU",
+credits: "Arifa Jahan Nuva",
 description: "Update team information quickly",
 envConfig: {
 sendNoti: true,
